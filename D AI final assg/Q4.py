6084b0b248ceb4ed5c7a5caccabefb5440b7e3ee
Prabha Venkatesh <prabha0106@gmail.com>
2022-04-11 18:52:52 +0100

    Adding all changes

--- D AI final assg/Q4.py
+++ D AI final assg/Q4.py
@@ -2,12 +2,9 @@ import random
 import numpy as np
 import math
 
-from soupsieve import select
-
 rows = 2
 cols = 4
 coffeeStates = 2    # without coffee = 0, with coffee = 1
-gridWorldCurrentUtilities = [[[0] * cols for x in range(rows)] for y in range(coffeeStates)] 
 numberStates = rows * cols * coffeeStates
 
 # 0.8 probability in the intended direction, and 0.1 probability to the right or left of the intended direction,
@@ -37,67 +34,73 @@ epsilon = 0.1
 discountFactor = 0.95
 alpha = 0.1
 
-class MDP:
-    def __init__(self, states, actions, rewards, alpha= 0.1, epsilon = 0.1, discountFactor = 0.95)-> None:
-        self.states = []
-        self.actions = list(dicActions.keys())
-
-def getQTableIndex( coffee, row, col) -> int:
-    #coffeeState * (rows * cols) + newX * cols + newY
+def getQTableIndex( coffee, row, col) -> int:   #Maps the gridworld row, col values and coffee states to the row index of Q table
     return coffee * (rows * cols) + row * cols + col
-
 
 def collectCoffeeRL(goal: int, fire: int, movement: int):
     # initialization  
+
     # reward allocation for terminal position
-    
-    gridWorldCurrentUtilities[withoutCoffeeState][coffeeX][coffeeY] = gridWorldCurrentUtilities[1][coffeeX][coffeeY] 
-    gridWorldCurrentUtilities[withoutCoffeeState][fireplaceX][fireplaceY]= fire
-    gridWorldCurrentUtilities[1-withoutCoffeeState][fireplaceX][fireplaceY]= fire  
-    gridWorldCurrentUtilities[1-withoutCoffeeState][officeX][officeY] = goal  
+    fireplaceIndex = getQTableIndex(withoutCoffeeState,fireplaceX,fireplaceY)
+    for col in range(len(Qtable[fireplaceIndex])):
+        Qtable[fireplaceIndex][col] = fire
+   
+    fireplaceIndex = getQTableIndex(1-withoutCoffeeState,fireplaceX,fireplaceY)
+    for col in range(len(Qtable[fireplaceIndex])):
+        Qtable[fireplaceIndex][col] = fire
+
+    officeIndex = getQTableIndex(1-withoutCoffeeState, officeX ,officeY)
+    for col in range(len(Qtable[officeIndex])):
+        Qtable[officeIndex][col] = goal
 
     startPosition = (startX, startY)
     startState = (startPosition, withoutCoffeeState)
 
     #TD Q-Learning algorithm
     iterCount = 0
-    while (iterCount< 10001):
-        currentState = startState #(withoutCoffeeState, startX, startY)
-
+    while (iterCount< 10001):        
+        currentState = startState #( (startX, startY), withoutCoffeeState)
         while (currentState not in terminalStates):
-            randExplorationVal = random.random()# TRY - random.uniform(0, 1) 
-
             (position, coffeeState) = currentState
             (row, col) = position
-            rowIndexQ = coffeeState * (rows * cols) + row * cols + col
+            rowIndexQ = getQTableIndex(coffeeState, row, col)
             
+            # update the Q values for coffee collection point in no Coffee state to get robot to move there
+            noCoffeeCollectIndex = getQTableIndex(withoutCoffeeState, coffeeX ,coffeeY)
+            coffeeCollectIndex = getQTableIndex(1-withoutCoffeeState, coffeeX ,coffeeY)
+            for col in range(len(Qtable[noCoffeeCollectIndex])):
+                Qtable[noCoffeeCollectIndex][col] = Qtable[coffeeCollectIndex][col]
+
+            # using Epsilon - greedy approach
+            randExplorationVal = random.random() # TRY - random.uniform(0, 1) 
+
             if (randExplorationVal < epsilon):
                 # PLAY RANDOM ACTION
                 randAction = random.random()
                 # if randAction = 1, last action ('W') must be chosen explicitly else error
+                # Follows North, South, East,  West 
                 colIndexQ = math.floor(randAction*4) if randAction != 1 else len(actionsOrdered)-1
-                #selectedAction = actionsOrdered[colIndexQ]
             else:
                 #PLAY BEST ACTION
                 stateQvalues = Qtable[rowIndexQ]
                 indices = [i for i, x in enumerate(stateQvalues) if x == max(stateQvalues)]
-                #selectedAction = actionsOrdered[colIndexQ]
                 # Calling random choice even if there is only 1 machine to sync the random state value with expected o/p
                 colIndexQ = random.choice(indices)  
             
-            oldValue = Qtable[rowIndexQ, colIndexQ]
-            actualDirIndex= colIndexQ
+            oldValue = Qtable[rowIndexQ, colIndexQ] # Q (s, a)
+            selectedActionIndex= colIndexQ   # action index
 
             # TRAVEL TO NEXT STATE
-            #  Part 1 :Reaching next state depends on robot probabilities
+            actualDirIndex = selectedActionIndex
+            # Part 1 :Reaching next state depends on robot probabilities
             randMovement = random.random()
             if randMovement > 0.8 and randMovement < 0.9:   #Moves LEFT of directions
-                actualDirIndex = 0 if actualDirIndex +1 ==len(actions) else actualDirIndex +1
+                actualDirIndex = selectedActionIndex-1
             elif randMovement >= 0.9:   #Moves RIGHT of directions
-                actualDirIndex = actualDirIndex-1
+                actualDirIndex =  0 if selectedActionIndex +1 ==len(actions) else selectedActionIndex +1
             selectedAction = actions[actualDirIndex]
             
-            # Part 1.1 update the position of the new state (temporary)
+            # Part 1.1 update the position based on the direction
             (newX,newY) = position 
             # X index changes only while going north and south
             if selectedAction == 'N' and newX!= 0:  # north => row -1
@@ -112,25 +115,31 @@ def collectCoffeeRL(goal: int, fire: int, movement: int):
             
             newPosition = (newX,newY)
             
-            # Part 2: Next state depends on if coffee was present or not
-            randCoffee = random.random()
-            if randCoffee >= 0.9 and coffeeState == 1:   #Has cofee and Drops Coffee
-                coffeeState = 1- coffeeState
+            # Part 2: Next state also depends on if coffee was present or not
+            if coffeeState == 1:   #Has cofee 
+                randCoffee = random.random()
+                if randCoffee >= 0.9:   # and Drops Coffee
+                    coffeeState = 1- coffeeState
 
-            newState = (newPosition, coffeeState)      #new state is acheived - factors = has coffee, directional probability and epsilon-greedy approach
+            newState = (newPosition, coffeeState) #new state is acheived - factors = has coffee, directional probability and epsilon-greedy approach
             newStateIndex =  getQTableIndex(coffeeState, newX, newY)  #  coffeeState * (rows * cols) + newX * cols + newY
-            nextMax = Qtable[newStateIndex, actualDirIndex]
 
             reward = movement
-            if newPosition in terminalPositions:
-                if newPosition == (fireplaceX, fireplaceY):
-                    reward = fire
-                elif coffeeState == 1 and newPosition == (coffeeX, coffeeY):# new position = office
-                    reward = goal
-                
+            if newPosition == (fireplaceX, fireplaceY):
+                reward = fire
+            elif newPosition == (officeX, officeY) and coffeeState == 1:# new position = office
+                reward = goal
+                    
+            #get indices (actions) in the new State which have highest Q values 
+            actionsIndexList = [i for i, x in enumerate(Qtable[newStateIndex]) if x == max(Qtable[newStateIndex])] 
+            selectedDirIndex = random.choice(actionsIndexList) if len(actionsIndexList)> 1 else actionsIndexList[0]  # if multiple, random choice
+            nextMaxQ = Qtable[newStateIndex, selectedDirIndex]
 
-            newValue = (1 - alpha) * oldValue + alpha * (reward + discountFactor * nextMax)
-            Qtable[newStateIndex, actualDirIndex] = newValue
+            #Q(s,a)= (1 - α) Q(s,a) + α(R(s) + λ max(Q(s', a')))
+            newValue = (1 - alpha) * oldValue + alpha * (reward + discountFactor * nextMaxQ)
+
+            # UPDATE THE OLD STATE VALUE
+            Qtable[rowIndexQ, colIndexQ] = newValue    #Update Q(s,a) with new value
             currentState = newState
         iterCount+=1
 
@@ -144,12 +153,13 @@ def collectCoffeeRL(goal: int, fire: int, movement: int):
                 position = (row, col)
                 state = (position, coffeeState)
  
-                if state in terminalStates:
+                if state in terminalStates or state in impossibleStates:
                     policyDir = '.'
                 else:
                     rowIndex = getQTableIndex(coffeeState, row, col)
-                    dirIndex = np.argmax(Qtable[rowIndex])
-                    policyDir = actions[dirIndex]
+                    dirIndexList = [i for i, x in enumerate(Qtable[rowIndex]) if x == max(Qtable[rowIndex])] #get indices which have highest Q values
+                    #dirIndexList = Qtable[rowIndex].indices  
+                    policyDir = actions[random.choice(dirIndexList)]    #random selection of best ones
 
                 colPolicy.append(policyDir)
             rowPolicy.append(colPolicy)
@@ -167,5 +177,17 @@ def collectCoffeeRL(goal: int, fire: int, movement: int):
             print(policies[1][row][col], end=" ")
         print("")
 
+# REF
+# https://www.learndatasci.com/tutorials/reinforcement-q-learning-scratch-python-openai-gym/
+
+# MOODLE gives time exceeded error -
+# output for 10000 runs is - 
+
+# No Coffee:
+# S . W .
+# S W W S
+# Coffee:
+# E E W .
+# S N E .
 random.seed(0)
 collectCoffeeRL(1,-1,0)
